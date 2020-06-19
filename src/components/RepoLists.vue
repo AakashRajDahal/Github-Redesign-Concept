a79dd40b4be2112bc2f73b935e30265f3585f265
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-19 23:52:32 +0545

    fontawesome cdn added to index.html and icons added to RepoLists.vue

--- src/components/RepoLists.vue
+++ src/components/RepoLists.vue
@@ -1,47 +1,47 @@
 <template>
   <div class="repo-lists">
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-lock"></i></span>
       <a class="lists-title">AakashRajDahal/eCommerce</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-code"></i></span>
       <a class="lists-title">TheRealRajan/Forum</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-code"></i></span>
       <a class="lists-title">Nischal10/RedShift</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-lock"></i></span>
       <a class="lists-title">sandeshpaudyal/FacebookBot</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-lock"></i></span>
       <a class="lists-title">markaa/VueJS Project</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-code"></i></span>
       <a class="lists-title">github/atom</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-lock"></i></span>
       <a class="lists-title">bprayush/bcmgmttool</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-lock"></i></span>
       <a class="lists-title">AakashRajDahal/Bootstrap4-Dashb...</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-code"></i></span>
       <a class="lists-title">TheRealRajan/ReactJS-Full-Stack-D...</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-code"></i></span>
       <a class="lists-title">Nischal10/TapWars</a>
     </aside>
     <aside class="lists">
-      <span>key</span>
+      <span><i class="fas fa-code"></i></span>
       <a class="lists-title">AakashRajDahal/CloudAnimationW...</a>
     </aside>
   </div>
