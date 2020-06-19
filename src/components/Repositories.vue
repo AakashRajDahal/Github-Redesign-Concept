fe67924c4e5b3e54ba0b46de1a8aea55f3a01c07
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-19 23:23:32 +0545

    SearchComponenrRepo.vue created and imported in Repositoires with css properties

--- src/components/Repositories.vue
+++ src/components/Repositories.vue
@@ -4,27 +4,33 @@
       <div class="g-title">Repositories</div>
       <Button />
     </aside>
+    <search-component-repo  class="search"/>
   </div>
 </template>
 
 <script>
 import Button from './Button.vue';
+import SearchComponentRepo from './SearchComponentRepo.vue';
 
 export default {
   components: {
     Button,
+    SearchComponentRepo,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.repo{
-background-color: #fff;
-padding: 1.4rem;
-border-radius: 5px;
-  .repo-title{
+.repo {
+  background-color: #fff;
+  padding: 1.4rem;
+  border-radius: 5px;
+  .repo-title {
     display: flex;
     justify-content: space-between;
   }
+  // .search{
+
+  // }
 }
 </style>
