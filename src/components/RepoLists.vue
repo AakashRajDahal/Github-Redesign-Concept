357b1b3c5471ffead5a4a071c5c946a8f3826460
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-19 23:41:08 +0545

    lists added to RepoLists.vue

--- src/components/RepoLists.vue
+++ src/components/RepoLists.vue
@@ -1,5 +1,63 @@
 <template>
-  <div>
-    <aside>key AakashRajDahal/Ecommerce</aside>
+  <div class="repo-lists">
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">AakashRajDahal/eCommerce</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">TheRealRajan/Forum</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">Nischal10/RedShift</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">sandeshpaudyal/FacebookBot</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">markaa/VueJS Project</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">github/atom</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">bprayush/bcmgmttool</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">AakashRajDahal/Bootstrap4-Dashb...</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">TheRealRajan/ReactJS-Full-Stack-D...</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">Nischal10/TapWars</a>
+    </aside>
+    <aside class="lists">
+      <span>key</span>
+      <a class="lists-title">AakashRajDahal/CloudAnimationW...</a>
+    </aside>
   </div>
 </template>
+
+<script>
+</script>
+
+<style lang="scss" >
+.repo-lists {
+  .lists {
+    .lists-title {
+      margin: 0 0.7rem;
+      color: #4078c0;
+      font-size: 16px;
+    }
+  }
+}
+</style>
