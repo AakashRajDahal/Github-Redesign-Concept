f3aee48e3bb4278397817bb4e36fa3e61dc8d2cc
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-21 22:57:42 +0545

    RepoTeam.vue created

---
+++ src/components/RepoTeam.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="repo-team">
+    <span class="repo-title">Working with a team?</span>
+    <p
+      class="repo-para"
+    >GitHub is built for collaboration. Set up an organization to improve the way your team works together, and get access to more features.</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" >
+.repo-team {
+  color: $primaryDark;
+  .repo-title {
+    font-size: 18px;
+  }
+  .repo-para {
+    font-size: 18px;
+  }
+}
+</style>
