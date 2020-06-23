a36bda4194aa11b5ba1e8b392d50ae3bcfde29e3
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-23 23:21:11 +0545

    padding-left added to .fas.fa-star

--- src/components/FeedCard.vue
+++ src/components/FeedCard.vue
@@ -15,23 +15,21 @@
     </aside>
     <aside class="card-status">
       <div class="status-title">
-        <span class="title-1">
-          edx/devstack
-        </span>
-        <span class="title-2">
-          Get up and running quickly to develop or extend Open edX services
-        </span>
+        <span class="title-1">edx/devstack</span>
+        <span class="title-2">Get up and running to develop or extend Open edX services</span>
       </div>
       <ButtonStar />
     </aside>
     <aside class="card-icons">
       <div class="icon-status">
-        <span><i class="fas fa-circle"></i> TypeScript</span>
-        <span><i class="fas fa-star"></i> 2.1k</span>
+        <span>
+          <i class="fas fa-circle"></i> TypeScript
+        </span>
+        <span>
+          <i class="fas fa-star"></i> 2.1k
+        </span>
       </div>
-      <div class="status">
-        New Repository
-      </div>
+      <div class="status">New Repository</div>
     </aside>
   </div>
 </template>
