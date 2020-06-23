ed71d62d2470dd4aa858f879459276f5eacb3a8e
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-23 22:13:28 +0545

    image added to assets

---
+++ src/components/FeedCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="card">
+        <aside class="card-title">
+            <div class="card-img">
+                <img src="./assets/Image 4.png" alt="">
+                <span></span>
+            </div>
+        </aside>
+    </div>
+</template>
