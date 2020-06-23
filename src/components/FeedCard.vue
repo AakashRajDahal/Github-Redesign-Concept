1d08894e44f9791b7a45884d98c1b422ffabd8ef
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-23 22:58:45 +0545

    font-size added to .status-title

--- src/components/FeedCard.vue
+++ src/components/FeedCard.vue
@@ -5,23 +5,32 @@
         <img src="@/assets/Image 4.png" alt />
         <div class="card-img-title">
           bprayush
-        <span>
-          forked
-          </span>
-         bprayush/devstack
-          <span>
-      from
-          </span>
+          <span>forked</span>
+          bprayush/devstack
+          <span>from</span>
           edx/devstack
         </div>
       </div>
-      <div class="card-time">
-        5 hours ago
+      <div class="card-time">5 hours ago</div>
+    </aside>
+    <aside class="card-status">
+      <div class="status-title">
+        <span class="title-1">
+          edx/devstack
+        </span>
+        <span class="title-2">
+          Get up and running quickly to develop or extend Open edX services
+        </span>
       </div>
+      <Button />
     </aside>
   </div>
 </template>
 
 <script>
+import Button from './Button.vue';
 
+export default {
+  Button,
+};
 </script>
