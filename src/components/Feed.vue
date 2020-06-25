879f61a6c34b2af072d07f5fa688410dc5ffd4d1
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-25 22:32:14 +0545

    contents added to FeedCard-2.vue

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="feed">
       <h2 class="feed-title"> Your <strong>Feed </strong></h2>
-        <feed-card />
+        <feed-card1 />
+        <feed-card2 />
     </div>
 </template>
 
 <script>
-import FeedCard from './FeedCard.vue';
+import FeedCard1 from './FeedCard-1.vue';
+import FeedCard2 from './FeedCard-2.vue';
+
 
 export default {
   components: {
-    FeedCard,
+    FeedCard1,
+    FeedCard2,
   },
 };
 </script>
