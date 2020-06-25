47361cfef1fec4c6ec3c9018e967a67d952a97dc
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-25 22:47:31 +0545

    css added to _feed-card.scss

--- src/components/FeedCard-2.vue
+++ src/components/FeedCard-2.vue
@@ -2,23 +2,20 @@
   <div class="card">
     <aside class="card-title">
       <div class="card-img">
-        <img src="@/assets/Image 4.png" alt />
+        <img src="@/assets/Image 6.png" alt />
         <div class="card-img-title">
-          bprayush
-          <span>forked</span>
-          bprayush/devstack
-          <span>from</span>
-          edx/devstack
+         kusumghimire
+          <span>pushed to</span>
+          AakashRajDahal/CloudAnimationWebsite
         </div>
       </div>
-      <div class="card-time">5 hours ago</div>
+      <div class="card-time">12 hours ago</div>
     </aside>
     <aside class="card-status">
-      <div class="status-title">
-        <span class="title-1">edx/devstack</span>
-        <span class="title-2">Get up and running to develop or extend Open edX services</span>
+      <div class="status-title button1">
+        <span class="title-2">4 commits to</span>
+      <button>master</button>
       </div>
-      <ButtonStar />
     </aside>
     <aside class="card-icons">
       <div class="icon-status">
