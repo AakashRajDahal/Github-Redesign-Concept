47fb0439af556a7587b8ac30eb22188fa934f374
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-26 22:38:04 +0545

    color added to icon in _explore-card.scss

--- src/components/ExploreCard-1.vue
+++ src/components/ExploreCard-1.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="card explore">
     <aside class="card-status">
-      <div class="status-title ">
-        <span class="title-1 explore">edx/devstack</span>
-        <span class="title-2">Get up and running to develop or extend Open edX services</span>
+      <div class="status-title">
+        <span class="title-1 explore">blitz-js/blitz</span>
+        <span class="title-2">
+          Rails-like framework for monolithic,
+          full-stack React apps — built on Next.js
+        </span>
       </div>
     </aside>
     <aside class="card-icons">
       <div class="icon-status">
         <span>
-          <i class="fas fa-circle"></i> TypeScript
+          <i class="fas fa-circle explore"></i> TypeScript
         </span>
         <span>
-          <i class="fas fa-star"></i> 2.1k
+          <i class="fas fa-star"></i> 2.4k
         </span>
       </div>
     </aside>
