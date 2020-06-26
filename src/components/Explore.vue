6a8ce8e40db7128d0d84270f5b9ceb8473e83472
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-26 22:25:46 +0545

    _explore-card.scss is created and imported in style.scss

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <h2>Explore Repositories</h2>
     <!-- <img src="@assets/Image-1.png" alt="person working " /> -->
+    <h2>Explore Repositories</h2>
+    <explore-card-1 />
   </div>
 </template>
 
 <script>
+import ExploreCard1 from './ExploreCard-1.vue';
+
+export default {
+  components: {
+    ExploreCard1,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/components/ExploreCard-1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card explore">
+    <aside class="card-status">
+      <div class="status-title">
+        <span class="title-1">edx/devstack</span>
+        <span class="title-2">Get up and running to develop or extend Open edX services</span>
+      </div>
+      <button>
+        <i class="fas fa-star"></i> Star
+      </button>
+    </aside>
+    <aside class="card-icons">
+      <div class="icon-status">
+        <span>
+          <i class="fas fa-circle"></i> TypeScript
+        </span>
+        <span>
+          <i class="fas fa-star"></i> 2.1k
+        </span>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script>
+</script>
