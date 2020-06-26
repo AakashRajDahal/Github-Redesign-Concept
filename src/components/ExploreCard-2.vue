9522c8e0d2fbd0bfae1b3711f0b70e1d58474301
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-06-26 22:45:29 +0545

    contents added to ExploreCards

---
+++ src/components/ExploreCard-2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card explore">
+    <aside class="card-status">
+      <div class="status-title">
+        <span class="title-1 explore">phuoc-ng/csslayout</span>
+        <span class="title-2">
+          A collection of popular layouts and patterns made with CSS.
+          Now it has 90+ patterns and continues growing!
+        </span>
+      </div>
+    </aside>
+    <aside class="card-icons">
+      <div class="icon-status">
+        <span>
+          <i class="fas fa-circle explore"></i> TypeScript
+        </span>
+        <span>
+          <i class="fas fa-star"></i> 2.4k
+        </span>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script>
+</script>
---
+++ src/components/ExploreCard-4.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card explore">
+    <aside class="card-status">
+      <div class="status-title">
+        <span class="title-1 explore">AakashRajDahal/Bootstrap4-Dashboard</span>
+        <span class="title-2">
+          Legend Agar.io Mod
+        </span>
+      </div>
+    </aside>
+    <aside class="card-icons">
+      <div class="icon-status">
+        <span>
+          <i class="fas fa-circle explore"></i> HTML
+        </span>
+        <span>
+          <i class="fas fa-star"></i> 24
+        </span>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script>
+</script>
